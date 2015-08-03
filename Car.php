b3a6aa2dc0c8bd4b4e357f2eac936c737e3f1aa1
Ian McKenney & Casey Heitz <student@epicodus.com>
2015-08-03 11:03:10 -0700

    Add getters and setters to Car.php

--- Car.php
+++ Car.php
@@ -1,32 +1,68 @@
 <?php
   class Car
   {
-    public $make_model;
-    public $price;
-    public $miles;
+    private $make_model;
+    private $price;
+    private $miles;
+
+    function __construct($car_model, $car_price, $car_miles)
+    {
+      $this->make_model = $car_model;
+      $this->price = $car_price;
+      $this->miles = $car_miles;
+    }
+
+    function setModel($car_model)
+    {
+      $this->make_model = $car_model;
+    }
+
+    function setPrice($car_price)
+    {
+      $this->price = $car_price;
+    }
+
+    function setMiles($car_miles)
+    {
+      $this->miles = $car_miles;
+    }
+
+    function getModel()
+    {
+      return $this->make_model;
+    }
+
+    function getPrice()
+    {
+      return $this->price;
+    }
+
+    function getMiles()
+    {
+      return $this->miles;
+    }
+
   }
 
-  $porche new Car();
-  $porche->make_model = "2014 Porche 911";
-  $porche->price = 114991;
-  $porche->miles = 7864;
 
-  $ford = new Car();
-  $ford->make_model = "2011 Ford F450";
-  $ford->price = 55995;
-  $ford->miles = 14241;
 
-  $lexus = new Car();
-  $lexus->make_model = "2013 Lexus RX 350";
-  $lexus->price = 44700;
-  $lexus->miles = 20000;
+  $porsche = new Car("2014 Porsche 911", 114991, 7864);
 
-  $mercedes = new Car();
-  $mercedes->make_model = "Mercedes Benz CLS550";
-  $mercedes->price = 39900;
-  $mercedes->miles = 37979;
+  $ford = new Car("2011 Ford F450", 55995, 14241);
 
-  $cars = array($porsche, $ford, $lexus, $mercedes)
+  $lexus = new Car("2013 Lexus RX 350", 44700, 20000);
+
+  $mercedes = new Car("Mercedes Benz CLS550", 39900, 37979);
+
+
+  $cars = array($porsche, $ford, $lexus, $mercedes);
+  $cars_matching_search = array();
+  foreach ($cars as $car) {
+    $price_data = $car->getPrice();
+    if ( $price_data <= $_GET["price"]) {
+      array_push($cars_matching_search, $car);
+    }
+  }
 ?>
 
 <!DOCTYPE html>
@@ -38,11 +74,14 @@
     <h1>Ian & Casey's Dealership-O'Rama</h1>
     <ul>
         <?php
-          foreach ($cars as $car) {
-            echo "<li> $car->make_model </li>";
+          foreach ($cars_matching_search as $car) {
+            $model_data = $car->getModel();
+            $price_data = $car->getPrice();
+            $miles_data = $car->getMiles();
+            echo "<li>$model_data</li>";
             echo "<ul>";
-              echo "<li> $$car->price </li>";
-              echo "<li> Miles:$car->miles </li>";
+              echo "<li> $$price_data </li>";
+              echo "<li> Miles:$miles_data</li>";
             echo "</ul>";
           }
         ?>
